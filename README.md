2d5544c53fbca1859db7edb5bd71140c9d28c62e
javirs200 <fordrsmax@gmail.com>
2023-10-17 09:13:16 +0200

    after class backup

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # jsFrontedApp
+
+Proyecto individual para la realización de una aplicación FrontEnd usnado los conocimientos de HTML, CSS, JavaScript aprendidos hasta el momento.
+
+Requisitos
+Manipulación dinámica del DOM
+Crear una página SPA para las preguntas
+Manejo de ES6
+Peticiones HTTP a APIs
+APIs HTML5: Uso de Local storage, gráficas, Mapas, etc...
+Uso de Firebase:Auth, Storage, Firestore
+Sin frameworks ni librerias externas en la medida de lo posible
+Gestión del proyecto con  Github 
+Código limpio, buenas prácticas
+README bien documentado.
+Desplegar la app en gitHub Pages y añadir URL al README
+
+
+Características generales de tu App:
+¿Qué tecnologías/APIs usarás en tu APP? Ejemplo:
+Para esta web usaremos:
+HTML
+CSS
+JavaScript
+ES6
+Gráficas?
+Mapas?
+¿API?¿Cuál/es?
+¿No encuentro API para la idea? Consúltanos
+¿Firebase? ¿Autenticación, Firestore, Sorage?
--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,37 @@
 //variables globales
+const mapboxApiToken = "access_token=pk.eyJ1IjoicnNtYXgiLCJhIjoiY2xudDB2NGNxMTdlZTJqbXoza2JyZnl0bSJ9.K0F_DzA9r6WvsgF4d6EEQw"
+
+let mapboxApiEndpoint = "https://api.mapbox.com/styles/v1/mapbox/streets-v12/static/"
 
 
+
+async function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition)
+  } else { 
+    console.log("Geolocation is not supported by this browser.");;
+  }
+}
+
+async function showPosition(position) {
+  console.log(position["coords"]);
+  let mapboxApiParams = "-122.4241,37.78,10,0,0/400x400?"
+  let url = mapboxApiEndpoint+mapboxApiParams+mapboxApiToken
+  console.log(url);
+  let response = await fetch(url);
+  let container = document.getElementById("mapa")
+  let img = document.createElement("img")
+  img.src = response
+  container.appendChild(img)
+}
+
+async function createMapboxApiParams(){
+  
+}
+
+async function getMap() {
+  
+}
 
 window.onload = () => {
     let boton = document.getElementById("accion");
@@ -8,7 +39,7 @@ window.onload = () => {
     if (boton != null) {
   
       boton.onclick = () => {
-  
+        getLocation()
       }
     }
   }
